26e11cddf6cbca37d651137b0723d0f6e81e9727
Peter <peteracosta@gmail.com>
2026-09-06 16:59:45 -0300

    Modificado en Dilma-Ubuntu 26.04

---
+++ 01-workbench-nginx-php-grunt/www/_test.php
@@ -0,0 +1,9 @@
+<?php /*! test.local - v1.0 - 02-09-2023 */
+
+// ------------------------------------------------------------------------- source: _auxiliar/zGrunt/PHP-functions/aux-1.php
+echo 'Hello world';
+
+
+// ------------------------------------------------------------------------- source: _auxiliar/zGrunt/PHP-functions/aux-2.php
+phpinfo();
+/* este es el pie */
---
+++ README.md
@@ -0,0 +1,4 @@
+# WorkBenches
+Entornos de desarrollo Docker con Nginx+PHP+node
+con task runner como Grunt o webpack
+
